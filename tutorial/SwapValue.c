f8754093ffbec3f76a4ac232fdbb93a572174601
Zulhaditya <zulhaditya@gmail.com>
2025-04-30 21:14:20 +0700

    UPDATE: swap value of two variables

---
+++ tutorial/SwapValue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char x[] = "water";
+  char y[] = "lemon";
+  char temp[15];
+
+  strcpy(temp, x);
+  strcpy(x, y);
+  strcpy(y, temp);
+
+  printf("x = %s\n", x);
+  printf("y = %s\n", y);
+
+  return 0;
+}
