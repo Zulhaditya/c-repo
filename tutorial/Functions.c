dc43c6b2811a5f8d3ebc79b65c775c752b3726f2
Zulhaditya <zulhaditya@gmail.com>
2025-04-30 09:14:53 +0700

    UPDATE: return statement

--- tutorial/Functions.c
+++ tutorial/Functions.c
@@ -8,6 +8,18 @@ void selamat(char x[], int y) {
   printf("Kamu sekarang sudah %d tahun!\n", y);
 }
 
+// return adalah pengembalian nilai
+// sesuaikan dengan nilai return nya. double untuk return double.
+// void digunakan jika tidak ada value yang di-return
+double persegi(double x) {
+  // cara biasa
+  // double hasil = x * x;
+  // return hasil;
+
+  // cara cepat
+  return x * x;
+}
+
 int main() {
 
   // variabel
@@ -16,6 +28,9 @@ int main() {
 
   // panggil fungsi selamat() dan jadikan variabel sebagai argumen
   selamat(nama, umur);
+
+  double x = persegi(3.14);
+  printf("Luas persegi adalah %.2lf\n", x);
 
   return 0;
 }
